ec0cd910855275890ee1d34fd9f1b2c6250ef4e2
Mel <joemelvin385@gmail.com>
2025-07-08 07:01:13 +0300

    Initial signup setup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
 import firebase_admin
@@ -10,14 +10,30 @@ cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route("/")
 def home():
-  return "Project start"
+  return render_template("login.html")
+
+@app.route("/admin_dashboard")
+def admin_dashboard():
+    return "<h2>Welcome, Admin</h2>"
+
+@app.route("/user_dashboard")
+def user_dashboard():
+    return "<h2>Welcome, User</h2>"
 
 @app.route("/test_db")
 def test_db():
     users_ref = db.collection("users").limit(1).stream()
     users = [doc.to_dict() for doc in users_ref]
     return {"status": "success", "data": users or "No users found"}
+
+@app.route("/add_test_user")
+def add_test_user():
+    db.collection("users").document("test_user").set({
+        "name": "Melvin",
+        "email": "melvin@example.com"
+    })
+    return {"status": "test user added"}
---
+++ static/js/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDod6LtGgM_Juucevytu8JSYjzKu-vCBzw",
+  authDomain: "flaskproject-98f71.firebaseapp.com",
+  projectId: "flaskproject-98f71",
+  storageBucket: "flaskproject-98f71.firebasestorage.app",
+  messagingSenderId: "82687394269",
+  appId: "1:82687394269:web:4f108fc12cd04147333634",
+  measurementId: "G-NKBTSHSZVB"
+};
+
+//Initialize firebase
+firebase.initializeApp(firebaseConfig);
