08683e07aba268c5192d899f4f1ed6be733b4cd2
Melvin <joemelvin385@gmail.com>
2025-07-08 05:28:49 +0300

    Connecting database

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+load_dotenv()
+cred_path = os.getenv("FIREBASE_CREDENTIALS")
+cred = credentials.Certificate(cred_path)
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
   return "Project start"
+
+@app.route("/test_db")
+def test_db():
+    users_ref = db.collection("users").limit(1).stream()
+    users = [doc.to_dict() for doc in users_ref]
+    return {"status": "success", "data": users or "No users found"}
